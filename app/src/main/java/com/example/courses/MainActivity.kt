b1e8917a74ee4f30acad8b8ced9f7ccfed49e5ae
Achraf Boujaanan <achrafboujaanan2004@gmail.com>
2024-01-16 12:31:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.courses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.courses.data.DataSource
+import com.example.courses.model.Course
+import com.example.courses.ui.theme.CoursesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoursesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CourseApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CourseApp() {
+    CourseList(
+        courseList = DataSource.topics,
+        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+    )
+}
+
+@Composable
+fun CourseList(courseList: List<Course>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+    ) {
+        items(courseList) {
+            CourseCard(course = it)
+        }
+    }
+}
+
+@Composable
+fun CourseCard(course: Course, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row {
+            Column {
+                Image(
+                    painter = painterResource(course.imageResourceId),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(dimensionResource(R.dimen.padding_image))
+                        .width(dimensionResource(R.dimen.padding_image)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column(
+                modifier = Modifier.padding(
+                    top = dimensionResource(R.dimen.padding_medium),
+                    end = dimensionResource(R.dimen.padding_medium)
+                )
+            ) {
+                Text(
+                    text = stringResource(course.stringResourceId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_small)
+                    )
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_medium))
+                    )
+                    Text(
+                        text = course.cantidadCursos.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_medium))
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CoursesTheme {
+        //CourseCard(course = Course(R.string.photography, 38, R.drawable.photography))
+        CourseApp()
+    }
+}
